200e1f366db5a6e2ea751c87810752675f525492
09B20035 <09B20035@localhost>
2022-04-07 13:10:46 +0900

    ThisIsAFirstCommit

---
+++ main.c
@@ -0,0 +1,10 @@
+/*
+ *  Copyright (c) 2022 Fuma Suenaga(mirroris on Github)
+ *  This software is released under the MIT License, see LICENSE.txt
+ */
+
+ #include <stdio.h>
+
+ int main(){
+    return 0;
+ }
